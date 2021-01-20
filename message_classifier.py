6008ca47db56f00fc2b8b8823fd63b9d4545ce22
shayxu-ai <yxu9428@163.com>
2021-01-20 14:57:50 +0800

    begin

---
+++ message_classifier.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Date: 2021/01/20 Wed
+# @Author: ShayXU
+# @Filename: message_classifier.py
+
+
+"""
+    定义报文分类器
+"""
+
+
+class message_classifier():
+    def __init__(self) -> None:
+        super().__init__()
+
+        
+
+
+
+
--- network_protocol_message_analyzer.py
+++ network_protocol_message_analyzer.py
@@ -6,29 +6,37 @@
 
 
 """
-    scapy 20分钟教程 https://github.com/secdev/scapy/blob/master/doc/notebooks/Scapy%20in%2015%20minutes.ipynb?short_path=7cc498d
+    scapy 20分钟教程 https://github.com/secdev/scapy/blob/master/doc/notebooks/Scapy%20in%2015%20minutes.ipynb
     Python中Scapy网络嗅探模块的使用 https://www.cnblogs.com/csnd/archive/2004/01/13/11807813.html
-    # https://pypi.org/project/scapy/
+    官方文档 https://pypi.org/project/scapy/
 
-    # netcat或Hping 也可以考虑
+    1、嗅探数据报文
+    2、通过定义报文特征，将报文划入不同分组。
+    3、不同分组代表不同的业务流程。
+    4、检验业务流程
 
     数据链路层/网络层/传输层/应用层
     Ether()/IP()/TCP()/HTTP()
 
     # filter 使用的是 Berkeley Packet Filter (BPF)语法
+    # netcat或Hping 也可以考虑
 """
 
 
-from scapy.all import *
+from scapy import main
+from scapy.all import IP, UDP, DNS, DNSQR, raw, rdpcap
 
 
-pkt = IP(dst="8.8.8.8")/UDP()/DNS(qd=DNSQR())
+# pkt = IP(dst="8.8.8.8")/UDP()/DNS(qd=DNSQR())
 # print(repr(raw(pkt)))
 # print(pkt.summary())
-pkt.canvas_dump()
 
 # ans, unans = traceroute('www.secdev.org', maxttl=15)
 # ans.world_trace()
 # 这个点很酷，只要知道这些ip归属哪台设备，就可以知道traceroute经过了哪些ip。
 
 # 是否加密(获取设备的密钥)，能不能嗅探
+
+if __name__ == "__main__":
+    filename = "朝歌组播机顶盒开机包.pcapng"
+    pcap_p = rdpcap(filename)
